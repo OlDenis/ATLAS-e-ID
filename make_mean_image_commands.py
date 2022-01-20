49bfe5e68b92a52f9dbf0ad8a84468903170d781
OlDenis <olivier.denis.1@umontreal.ca>
2022-01-20 13:30:07 -0500

    adjusted str delimiters

--- make_mean_image_commands.py
+++ make_mean_image_commands.py
@@ -11,11 +11,9 @@ pt_regions = [("5","20"), ("30","35"), ("90","130")]
 command_list = []
 for eta in eta_regions:
     for pt in pt_regions:
-        eta_region = eta[0] + "-" + eta[1]
-        CUTS = '(abs(sample["eta"]) > {0}) & (abs(sample["eta"]) < {1}) \\
-                & (sample["pt"] > {2}) & (sample["pt"] < {3})'.format(*eta, *pt)
-        COMMAND = """python classifier.py --host_name=beluga --n_classes=0\\
-                --n_train=0 --valid_cut={} --eta_region={}""".format(CUTS, eta_region)
+        eta_region = "'" + eta[0] + "-" + eta[1] + "'"
+        CUTS = """'(abs(sample["eta"]) > {0}) & (abs(sample["eta"]) < {1}) & (sample["pt"] > {2}) & (sample["pt"] < {3})'""".format(*eta, *pt)
+        COMMAND = """python classifier.py --host_name=beluga --n_classes=0 --n_train=0 --valid_cut={} --eta_region={}""".format(CUTS, eta_region)
         command_list.append(COMMAND)
 
 command_list.append("exit")
